a145df02b101829d5804f94c178591e28ef88c96
Wayne Krug <wmkrug@gmail.com>
2014-06-16 19:02:29 -0400

    Added code to read ROM file data and identify memory type

--- hexdumper.c
+++ hexdumper.c
@@ -19,6 +19,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "constants.h"
+#include "rom_structs.h"
+#include "util.h"
+
 int main(const int argc, const char* argv[])
 {
   if (argc < 2)
@@ -30,6 +34,24 @@ int main(const int argc, const char* argv[])
 
   const char* rom_filename = argv[1];
   const FILE* rom_file = fopen(rom_filename);
+
+  // Read all data and verify successful read
+  char *raw_rom_data = malloc(MAX_ROM_SIZE);
+  const size_t bytes_read = fread(raw_rom_data, 1, MAX_ROM_SIZE, rom_file);
+
+  // Either bytes_resd should be  MAX_ROM_SIZE, or it should be less and
+  // rom_file should be at EOF
+  if (bytes_read < MAX_ROM_SIZE)
+  {
+    if (feof(rom_file) == 0)
+    {
+      printf("Error reading file %s\n", rom_filename);
+      // TODO add error code?
+      return -1;
+    }
+  }
+
+  const ROM_TYPE_t rom_type = id_ROM_type(raw_rom_data);
 
   return 0;
 }
